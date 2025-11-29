e9722e73102617697cf899f26559fb6de1c754f1
Pentasis <7046024+Pentasis@users.noreply.github.com>
2025-11-29 12:18:12 +0100

    Cleanup ModSystem

--- src/BonfiresMod.cs
+++ src/BonfiresMod.cs
@@ -1,11 +1,4 @@
-﻿using Vintagestory.API.Client;
-using Vintagestory.API.Common;
-using Vintagestory.API.Server;
-
-[assembly: ModInfo( "Bonfires",
-	Description = "Adds Bonfires",
-	Website     = "https://github.com/copygirl/howto-example-mod",
-	Authors     = new []{ "Zarkonnen" } )]
+﻿using Vintagestory.API.Common;
 
 namespace Bonfires
 {
@@ -13,19 +6,8 @@ namespace Bonfires
 	{
 		public override void Start(ICoreAPI api)
 		{
-			//api.RegisterBlockBehaviorClass(InstaTNTBehavior.NAME, typeof(InstaTNTBehavior));
 			api.RegisterBlockEntityClass("BlockEntityBonfire", typeof(BlockEntityBonfire));
 			api.RegisterBlockClass("BlockBonfire", typeof(BlockBonfire));
-		}
-		
-		public override void StartClientSide(ICoreClientAPI api)
-		{
-			
-		}
-		
-		public override void StartServerSide(ICoreServerAPI api)
-		{
-			
 		}
 	}
 }
